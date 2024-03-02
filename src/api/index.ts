ebb21807e803d39f000209249a48b9ee6b9d2a39
Kirill <krancheh@gmail.com>
2024-03-03 00:58:51 +0300

    fix auth header & error handling

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,9 +14,9 @@ $api.interceptors.request.use((config) => {
     config.data = JSON.stringify(config.data);
     const date = new Date();
 
-    const year = date.getFullYear().toString();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
+    const year = date.getUTCFullYear().toString();
+    const month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+    const day = ('0' + date.getUTCDate()).slice(-2);
 
     // md5(pwd_20240301)
     config.headers["X-Auth"] = CryptoJS.MD5(`${process.env.PASSWORD}_${year + month + day}`);  // Auth header
@@ -27,13 +27,14 @@ $api.interceptors.request.use((config) => {
 // Response interceptor
 $api.interceptors.response.use(
     (response) => {
-        response.data = JSON.parse(response.data);
+        try {
+            response.data = JSON.parse(response.data);
+        } catch (error) {
+            console.log(response.data);
+        }
 
         return response;
-    },
-    // (error: any) => {
-    //     console.log(error);
-    // }
+    }
 )
 
 
