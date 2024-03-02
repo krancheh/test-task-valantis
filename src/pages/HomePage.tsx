728c9fb25fdeefb70e62cc3eec63a32976d84fc3
Kirill <krancheh@gmail.com>
2024-03-02 15:43:42 +0300

    add Home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+    return (
+        <div className="hero">
+            <h1 style={{ marginBottom: "15px" }}>Добро пожаловать</h1>
+            <Link className='button' to="/items">Перейти в каталог</Link>
+        </div>
+    )
+}
+
+export default HomePage
