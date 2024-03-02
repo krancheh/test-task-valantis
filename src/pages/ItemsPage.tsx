4f093bc9e3bbde3307f4b5dcf8f7e6693473bc91
Kirill <krancheh@gmail.com>
2024-03-03 00:57:34 +0300

    add ItemsPage

---
+++ src/pages/ItemsPage.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useMemo, useState } from "react"
+import ItemList from "../components/ItemList/ItemList"
+import useFetchItems from "../hooks/useFetchItems"
+import { URLSearchParamsInit, useSearchParams } from "react-router-dom"
+import $api from "../api"
+import { ResponseData } from "../types"
+
+const ItemsPage = () => {
+    const [searchParams, setSearchParams] = useSearchParams({});
+
+    const [offset, setOffset] = useState(Number(searchParams.get("offset")) || 0);
+    const [limit, setLimit] = useState(Number(searchParams.get("limit")) || 50);
+
+    const paginationModel = useMemo(() => ({ offset, limit }), [offset, limit])
+    const [items, isSuccess] = useFetchItems({ paginationModel });
+
+
+    useEffect(() => {
+        const newSearchParams: URLSearchParamsInit = {
+            offset: offset.toString(),
+            limit: limit.toString(),
+        }
+
+        setSearchParams(newSearchParams)
+        $api.post<ResponseData<string[]>>("/", { action: "get_fields", params: { field: "brand" } })
+            .then(response => {
+                const fields = new Set(response.data.result);
+                console.log(Array.from(fields));
+
+            }
+            )
+    }, [offset, limit])
+
+    return (
+        <div>
+            <h1>Каталог товаров</h1>
+            {!isSuccess ? <h2>Загрузка..</h2> : <ItemList items={items} />}
+        </div>
+    )
+}
+
+export default ItemsPage
