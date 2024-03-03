71a912b7e06bdcd93d796269bdd2a9f4f3b5cdbc
Kirill <krancheh@gmail.com>
2024-03-03 12:36:26 +0300

    add redux store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 );
 
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
 
